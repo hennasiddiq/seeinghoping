57cd99fc0e9f6f18809e6821ff372b5442f15bb9
Amber Ross <ross.amber1540@gmail.com>
2019-07-17 15:59:54 -0700

    First save

--- seeinghoping/ViewController.swift
+++ seeinghoping/ViewController.swift
@@ -10,9 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    
+//    let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+//    ac.addTextField()
+//
+//    let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned ac] _ in
+//        let answer = ac.textFields![0]
+//        // do something interesting with "answer" here
+//    }
+//
+//    ac.addAction(submitAction)
+//    present(alert, animated: true)
+//
+//    var alertController = UIAlertController(title: "What's your name?", message: nil, preferredStyle: UIAlertController.Style.alert)
+//    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//
+    @IBAction func relaxButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func healthButton(_ sender: Any) {
+    }
+    
+    @IBAction func inspireButton(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let alert = UIAlertController(title: "What's your name?", message: nil, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        alert.addTextField(configurationHandler: { textField in textField.placeholder = "Input your name here..."
+        })
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            
+            if let name = alert.textFields?.first?.text {
+                print("Welcome: \(name)")
+            }
+        }))
+        
+        self.present(alert, animated: true)
     }
 
 
